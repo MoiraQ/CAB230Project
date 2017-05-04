24aacd9395b992fe5551e164718f97370415bea5
McKennaEB <mckenna.b@hotmail.com>
2017-05-05 09:22:53 +1000

    updated validation on login

--- CAB230Project/login.php
+++ CAB230Project/login.php
@@ -19,16 +19,19 @@
 	 <a href="search.html">Search</a><a href="resultspage.html">See All</a> <a href="userregpage.html">Sign Up</a> <a href="login.php"> Login </a>
 	 </div>
 	 
-	 <div id=image-container>
+	<div id=image-container>
 		<div id=form-container>
 		
 			<h2>Login</h2>
 			
-			<form action="welcome.php" method="post">
+			<form style="text-align:right !important" onsubmit="return validate()">
 			
 			Username: <input id="username" type="text" name="username"> <br>
+			<span id="usernameMissing" class="error-message"> Field Required</span><br><br>
 			
 			Password: <input type="password" name="password" id="password"> <br>
+			<span id="passwordMissing" class="error-message"> Field Required</span><br>
+			<br>
 			
 			<input type="submit" value="Login">
 		
