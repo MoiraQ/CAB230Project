ff4b09970fa1d6280b2feb882ec334f3470e3d13
McKennaEB <mckenna.b@hotmail.com>
2017-05-05 09:16:49 +1000

    login.php
    added login.php as per Section 4. Stage 2 point #2 - no functionality to the page yet, only structure of the login page

---
+++ CAB230Project/login.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<link href="mystyle.css" rel="stylesheet" type="text/css"/> 
+	<script type="text/javascript" src="javascript.js"></script>
+	
+
+	<title>Project - Review Website</title>
+	<!-- Moira Quinn & McKenna Buckley -->
+ <body>
+  
+
+	 <div id=header>
+		<h1>Brisbane Reviews</h1>
+	 </div>
+	 
+	 <div id=menu>
+	 <a href="search.html">Search</a><a href="resultspage.html">See All</a> <a href="userregpage.html">Sign Up</a> <a href="login.php"> Login </a>
+	 </div>
+	 
+	 <div id=image-container>
+		<div id=form-container>
+		
+			<h2>Login</h2>
+			
+			<form action="welcome.php" method="post">
+			
+			Username: <input id="username" type="text" name="username"> <br>
+			
+			Password: <input type="password" name="password" id="password"> <br>
+			
+			<input type="submit" value="Login">
+		
+			</form>
+		
+		</div>
+	</div>
+	
+	<div id=footer>
+		<p>Copyright text</p>
+		<p> Privacy Policy || Terms and Conditions </p>
+	</div>
+		
+		
+	</body>
+	
+</html>
