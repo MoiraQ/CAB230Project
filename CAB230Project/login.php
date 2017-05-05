4c30a59a47b20f6943b4d2f4f28c995c62ea46cf
McKennaEB <mckenna.b@hotmail.com>
2017-05-05 10:34:20 +1000

    Updated to php file
    swapped to php

--- CAB230Project/login.php
+++ CAB230Project/login.php
@@ -4,36 +4,39 @@
 	
 	<link href="mystyle.css" rel="stylesheet" type="text/css"/> 
 	<script type="text/javascript" src="javascript.js"></script>
-	
-
 	<title>Project - Review Website</title>
 	<!-- Moira Quinn & McKenna Buckley -->
  <body>
-  
-
-	 <div id=header>
+ <?php
+	<div id=header>
 		<h1>Brisbane Reviews</h1>
-	 </div>
+	</div>
 	 
-	 <div id=menu>
-	 <a href="search.html">Search</a><a href="resultspage.html">See All</a> <a href="userregpage.html">Sign Up</a> <a href="login.php"> Login </a>
-	 </div>
+	<div id=menu>
+		<a href="search.html">Search</a>
+		<a href="resultspage.html">See All</a>
+		<a href="userregpage.html">Sign Up</a> 
+		<a href="login.php"> Login </a>
+	</div>
 	 
 	<div id=image-container>
 		<div id=form-container>
-		
 			<h2>Login</h2>
 			
 			<form style="text-align:right !important" onsubmit="return validate()">
-			
-			Username: <input id="username" type="text" name="username"> <br>
-			<span id="usernameMissing" class="error-message"> Field Required</span><br><br>
-			
-			Password: <input type="password" name="password" id="password"> <br>
-			<span id="passwordMissing" class="error-message"> Field Required</span><br>
-			<br>
-			
-			<input type="submit" value="Login">
+				
+				Username: <input id="username" type="text" name="username"> 
+				<br>
+				<span id="usernameMissing" class="error-message"> Field Required</span>
+				<br>
+				<br>
+				
+				Password: <input type="password" name="password" id="password"> 
+				<br>
+				<span id="passwordMissing" class="error-message"> Field Required</span>
+				<br>
+				
+				<input type="submit" value="Login">
 		
 			</form>
 		
@@ -44,7 +47,7 @@
 		<p>Copyright text</p>
 		<p> Privacy Policy || Terms and Conditions </p>
 	</div>
-		
+?>
 		
 	</body>
 	
